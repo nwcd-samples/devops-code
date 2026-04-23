675db17258f5052aaa3d111240041da2c9a24cf2
thangzhongyan <thangzhongyan@163.com>
2026-04-23 18:17:00 +0800

    Update UserServiceImpl.java

--- src/main/java/com/aws/nwcd/devops/service/impl/UserServiceImpl.java
+++ src/main/java/com/aws/nwcd/devops/service/impl/UserServiceImpl.java
@@ -5,8 +5,8 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
-    private static String USER_NAME="devops";
-    private static String PASS_WORD="devops1013";
+    private static String USER_NAME="****";
+    private static String PASS_WORD="****";
 
     public boolean validUser(String userName){
         boolean flag = false;
